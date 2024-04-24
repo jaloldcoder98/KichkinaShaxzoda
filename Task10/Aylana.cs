80171ebfb28dddc3f198d11756e6b120f1766e46
Jaloliddin <jaloldcoder@gmail.com>
2024-04-24 09:58:07 +0500

    first commit

---
+++ Task10/Aylana.cs
@@ -0,0 +1,12 @@
+namespace CSharpModule.Task10;
+
+public class Aylana
+{
+    public Nuqta Markaz { get; }
+    public int Radius { get; }
+    public Aylana(Nuqta markaz, int radius)
+    {
+        Markaz = markaz;
+        Radius = radius;
+    }
+}
---
+++ Task10/Nuqta.cs
@@ -0,0 +1,7 @@
+namespace CSharpModule.Task10;
+
+
+public class Nuqta
+{
+    
+}
