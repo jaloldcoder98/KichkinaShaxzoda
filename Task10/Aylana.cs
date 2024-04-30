9694a888928567237022abcbfbd638877f941a0c
Jaloliddin <77582341+jaloldcoder98@users.noreply.github.com>
2024-04-30 16:51:04 +0500

    Update Aylana.cs

--- Task10/Aylana.cs
+++ Task10/Aylana.cs
@@ -3,8 +3,12 @@ namespace CSharpModule.Task10;
 public struct Aylana
 {
     public Nuqta Markaz { get; set; }
+    
     public int Radius { get; set; }
+    
     public Aylana(Nuqta markaz, int radius) => (Markaz, Radius) = (markaz, radius);
+    
     public bool IsIn(Nuqta nuqta) => Markaz.DistanceTo(nuqta) <= Radius;
+
     public override string ToString() => $"Markaz: {Markaz}, Radius: {Radius}";
 }
